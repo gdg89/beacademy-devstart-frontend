3c77aa7e646381ba25769acee907257e85d1e64a
Giuliano Garcia <giuilianogarcia2826@gmail.com>
2022-05-17 19:10:35 -0300

    Tipo de dados

---
+++ javascript/tipo-dados.js
@@ -0,0 +1,66 @@
+
+// Comentar em  linea
+
+/*Comentario 
+    em 
+    bloco 
+    */
+
+    //TIPO STRING
+console.log('Olha eu no "aspas" simples!!')// Simples entre duplas/ duplas entre simples.
+
+console.log(` 'Olha' "eu" no crase`)// aceita ambos tipo de aspas.
+
+    // TIPO NUMBER
+     
+    console.log(12)// integer
+    console.log(24.5)// float
+    console.log(Infinity)
+    console.log(12*24/4)// calc
+    console.log(50+(53*4.5))//calc
+    console.log(Math.round(50+(53*4.5)))
+
+
+    //TIPO BOOLEAN
+
+    console.log(42 < 56)//TRUE
+    console.log(42 >100)// FALSE
+
+    // TIPO INDEFINED
+
+    /* nao existe*/
+
+    // TIPO NULL 
+    /*Existe mas nao esta definido */
+
+
+
+    // OBJETOS
+     /* Armazena:
+     --> Prpiedades / Atributos
+     --> Funcionlidades / Metodos
+     {propiedade:"valor"}
+     */
+
+        console.log({
+            marca:'FIAT',
+            largura:'550cm',
+            peso:1,
+            velocidade:(90*200)*2,
+            andar: function(){
+                console.log(`Sua velocidade é de 200`)
+            },
+            possiuAr:true
+        })
+
+
+// TIPO ARRAY (Vetores)
+
+    /* 
+    --> Lista de valores
+    --> argumento de dados
+    */
+
+    ["carro",32,"moto"]
+            /* LISTA DE compras */
+            console.log(["bolacha","coca-cola","chocolate"])
